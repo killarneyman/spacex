a00fad8e6fa726cce009f427033ce78eab40c604
killarneyman <killarneyman@gmail.com>
2021-06-13 18:10:02 +0100

    Initial API work

--- app/Http/Controllers/SpaceXController.php
+++ app/Http/Controllers/SpaceXController.php
@@ -2,15 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
+use GuzzleHttp\Client;
 use App\Http\Controllers\Controller;
 
 class SpaceXController extends Controller
 {
+    private const URL = 'https://api.spacexdata.com/v3/';
+    
+    private $client;
+
     public function index($year = null)
     {
+        $this->client = new Client([
+            'base_uri' => self::URL,
+            'http_errors' => true,
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+        ]);
+
+        $results = $this->client->get('launches');
+
+        if($results->getStatusCode() == '200') {
+            $response = $results->getBody()->getContents();
+
+            return response($response)->header('Content-Type', 'application/json');
+        }
+
         return response()->json([
-            'success' => true
+            'success' => false
         ]);
     }
 }
